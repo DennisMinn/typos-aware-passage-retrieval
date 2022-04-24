cd6a8551294010ccf2654c4bac332f657fd29caf
Dennis Minn <minndennis@gmail.com>
2022-04-24 16:01:22 -0400

    updated BaseModel for pytorch lightning

--- base/base_model.py
+++ base/base_model.py
@@ -1,9 +1,12 @@
-import torch.nn as nn
-import numpy as np
+import os
+import tqdm
+
+import pytorch_lightning as pl
 from abc import abstractmethod
 
+from dataset.triples import TriplesDataset
 
-class BaseModel(nn.Module):
+class BaseModel(pl.LightningModule):
     """
     Base class for all models
     """
@@ -14,3 +17,26 @@ class BaseModel(nn.Module):
         :return: Model output
         """
         raise NotImplementedError
+
+    @abstractmethod
+    def training_step(self, batch, batch_idx):
+        """
+        Training logic for one batch
+        :return: dict containing loss and log
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def validation_step(self, batch, batch_idx):
+        """
+        Training logic for one batch in validation set
+        """
+        raise NotImplementedError
+
+ 
+    def configure_optimizers(self):
+        """
+        initializes optimizers
+        """
+        optimizer = torch.optim.Adam(self.parameters(), lr=config['lr'])
+        return optimizer
