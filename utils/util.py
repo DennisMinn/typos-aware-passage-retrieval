8ee8937d8eb638bccfef2f7a97f3fb11db7ae8b3
Dennis Minn <minndennis@gmail.com>
2022-04-26 14:01:37 -0400

    added additional read files

--- utils/util.py
+++ utils/util.py
@@ -1,5 +1,7 @@
-import pandas as pd
+import csv
 import tqdm as tqdm
+
+from collections import defaultdict
 
 def read_input(chunks = None, chunk_size = 5000, **params):
     #TODO 
@@ -35,3 +37,51 @@ def read_triples(path, chunks=None, chunk_size=5000):
     }
     
     return read_input(**params)
+
+def read_qidpidtriples(path):
+    triples = []
+    with open(qidpidtriples_path, 'r', encoding='latin-1') as infile:
+        for qid, pos_pid, neg_pid in reader:
+            triples.append({'qid':qid,
+                            'pos_pid':pos_id,
+                            'neg_pid':neg_pid})
+    return triples
+
+def read_queries(queries_path):
+    queries = dict()
+    with open(queries_path, 'r', encoding='latin-1') as infile:
+        reader = csv.reader(infile, delimiter='\t')
+        for qid, query in reader:
+            queries[int(qid)] = query
+    
+    return queries
+
+def read_collection(collection_path):
+    collection = dict()
+    with open(collection_path, 'r', encoding='latin-1') as infile:
+        reader = csv.reader(infile, delimiter='\t')
+        for pid, passage in reader:
+            collection[int(pid)] = passage
+
+    return collection
+
+def read_qrels(qrels_path):
+    qrels = []
+    with open(qrels_path, 'r', encoding='latin-1') as infile:
+        reader = csv.reader(infile, delimiter='\t')
+        for qid, _, pid, _ in reader:
+            qrels.append({'qid': qid, 'pid': pid})
+
+    return qrels
+
+def read_top1000(top1000_path):
+    top1000 = []
+    with open(top1000_path, 'r', encoding='latin-1') as infile:
+        reader = csv.reader(infile, delimiter='\t')
+        for qid, pid, query, passage in reader:
+            top1000.append({'qid': qid,
+                            'pid': pid,
+                            'passage': passage,
+                            'query': query})
+
+    return top1000
