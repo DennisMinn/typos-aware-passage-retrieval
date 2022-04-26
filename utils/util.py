65fb9dc1ab1cc214815de2746331538ad1a0aca7
Dennis Minn <minndennis@gmail.com>
2022-04-25 23:22:05 -0400

    added print statements

--- utils/util.py
+++ utils/util.py
@@ -10,10 +10,12 @@ def read_input(chunks = None, chunk_size = 5000, **params):
     df = pd.DataFrame()
     file_path = params['filepath_or_buffer']
     
+    print('Counting lines')
     n_rows = chunks * chunk_size if chunks is not None else sum(1 for row in open(file_path, 'rb'))
     params['chunksize'] = chunk_size
     params['low_memory'] = False
     
+    print('Parsing lines')
     with tqdm.tqdm(total = n_rows, desc = 'Reading Chunks: ') as progress_bar:
         for i, chunk in enumerate(pd.read_csv(**params)):
             df = pd.concat([df, chunk])
@@ -32,4 +34,4 @@ def read_triples(path, chunks=None, chunk_size=5000):
 	'compression': 'infer',
     }
     
-    return read_input(chunks, chunk_size, **params)
+    return read_input(**params)
